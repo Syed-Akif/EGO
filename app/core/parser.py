da2d6ab03ce57f74f0a90f29aa94a440443df7ca
Syed-Akif <sydakifuddin47@gmail.com>
2026-07-21 18:20:07 +0530

    Implemented desktop application launcher

--- app/core/parser.py
+++ app/core/parser.py
@@ -2,9 +2,6 @@ from app.core.command import Command
 
 
 class Parser:
-    """
-    Converts raw user input into a Command object.
-    """
 
     def parse(self, text: str) -> Command:
 
@@ -15,6 +12,10 @@ class Parser:
         intent = parts[0].lower()
 
         arguments = parts[1:]
+
+        print("DEBUG")
+        print("Intent:", intent)
+        print("Arguments:", arguments)
 
         return Command(
             raw=text,
